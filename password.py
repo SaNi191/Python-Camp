59cbd253bb65c27fc745fb8ef8e721161316ecb0
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2024-07-10 10:34:53 -0600

    upload
    upload the code

--- password.py
+++ password.py
@@ -3,8 +3,18 @@ username = 'Sam'
 user_name = input("Username: ")
 user_pass = int(input("Password: "))
 
-
+#austin_is_cool = False
+#austin_is_bad = True
 if user_pass == password and user_name == username:
     print("You logged in!")
+    logged_in = True
 else:
     print("Incorrect!")
+    logged_in = False
+
+if logged_in:
+    choice = input("Withdraw (w) or Deposit (d): ")
+    
+
+else:
+    print("You did not log-in")
